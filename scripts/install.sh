6cca07f2826edab38957c8ae843ce9adf878ffea
leekt0321 <leekt0321@naver.com>
2024-06-26 10:09:39 +0000

    test

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum -y install npm
 sudo yum -y install httpd
-sudo cat << EOF > /etc/httpd/conf/httpd.conf
+sudo cat << EOF >> /etc/httpd/conf/httpd.conf
 DocumentRoot "/home/ec2-user/startbootstrap-shop-homepage/dist"
 <Directory "/home/ec2-user/startbootstrap-shop-homepage/dist">
     Options Indexes FollowSymLinks
