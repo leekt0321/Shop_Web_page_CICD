332fe520b05916b3e3440c5e60a824a59c39e36a
leekt0321 <leekt0321@naver.com>
2024-06-27 07:08:00 +0000

    test

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,12 +4,29 @@ sudo yum -y install npm
 sudo yum -y install httpd
 
 CONFIG_FILE="/etc/httpd/conf/httpd.conf"
-BLOCK='DocumentRoot "/home/ec2-user/startbootstrap-shop-homepage/dist"
-<Directory "/home/ec2-user/startbootstrap-shop-homepage/dist">
-    Options Indexes FollowSymLinks
-    AllowOverride None
-    Require all granted
-</Directory>'
+BLOCK='<VirtualHost *:80>
+    # Proxy 설정
+    ProxyRequests Off
+    ProxyPreserveHost On
+    <Proxy *>
+        Order deny,allow
+        Allow from all
+    </Proxy>
+    ProxyPass /app/ http://internal-Project-WAS-LB-1758617147.ap-northeast-2.elb.amazonaws.com:8080/ disablereuse=on
+    ProxyPassReverse /app/ http://internal-Project-WAS-LB-1758617147.ap-northeast-2.elb.amazonaws.com:8080/
+
+    # Static 파일 설정
+    DocumentRoot "/home/ec2-user/startbootstrap-shop-homepage/dist"
+    <Directory "/home/ec2-user/startbootstrap-shop-homepage/dist">
+        Options Indexes FollowSymLinks
+        AllowOverride None
+        Require all granted
+    </Directory>
+    
+    # ProxyPass를 특정 경로로 제한하여 Static 파일과의 충돌을 방지
+    ProxyPass / http://internal-Project-WAS-LB-1758617147.ap-northeast-2.elb.amazonaws.com:8080/
+    ProxyPassReverse / http://internal-Project-WAS-LB-1758617147.ap-northeast-2.elb.amazonaws.com:8080/
+</VirtualHost>'
 
 # 블록이 이미 있는지 확인
 if ! grep -q "$BLOCK" "$CONFIG_FILE"; then
