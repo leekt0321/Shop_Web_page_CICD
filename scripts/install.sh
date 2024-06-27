15791fae49586e523dc573fdf58757cd5f129902
leekt0321 <leekt0321@naver.com>
2024-06-27 06:33:23 +0000

    test

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+
 sudo yum -y install npm
 sudo yum -y install httpd
-sudo cat << EOF >> /etc/httpd/conf/httpd.conf
-DocumentRoot "/home/ec2-user/startbootstrap-shop-homepage/dist"
+
+CONFIG_FILE="/etc/httpd/conf/httpd.conf"
+BLOCK='DocumentRoot "/home/ec2-user/startbootstrap-shop-homepage/dist"
 <Directory "/home/ec2-user/startbootstrap-shop-homepage/dist">
     Options Indexes FollowSymLinks
     AllowOverride None
     Require all granted
-</Directory>
-EOF
+</Directory>'
+
+# 블록이 이미 있는지 확인
+if ! grep -q "$BLOCK" "$CONFIG_FILE"; then
+    echo "$BLOCK" | sudo tee -a "$CONFIG_FILE" > /dev/null
+    echo "Configuration block added to $CONFIG_FILE"
+else
+    echo "Configuration block already exists in $CONFIG_FILE"
+fi
+
