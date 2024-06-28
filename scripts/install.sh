9aa7fde8fc9b65a2ada865357859d47f24a3d07d
leekt0321 <leekt0321@naver.com>
2024-06-28 01:49:03 +0000

    test

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,8 +13,8 @@ BLOCK='<VirtualHost *:80>
         Allow from all
     </Proxy>
     RedirectMatch 301 ^/app$ /app/
-    ProxyPass /app/ http://internal-Project-WAS-LB-1758617147.ap-northeast-2.elb.amazonaws.com:8080/ disablereuse=on
-    ProxyPassReverse /app/ http://internal-Project-WAS-LB-1758617147.ap-northeast-2.elb.amazonaws.com:8080/
+    ProxyPass /app http://internal-Project-WAS-LB-1758617147.ap-northeast-2.elb.amazonaws.com:8080/ disablereuse=on
+    ProxyPassReverse /app http://internal-Project-WAS-LB-1758617147.ap-northeast-2.elb.amazonaws.com:8080/
 
     # Static 파일 설정
     DocumentRoot "/home/ec2-user/startbootstrap-shop-homepage/dist"
@@ -23,12 +23,11 @@ BLOCK='<VirtualHost *:80>
         AllowOverride None
         Require all granted
     </Directory>
-
 </VirtualHost>'
 
-if grep -q "<VirtualHost *:80>" "$CONFIG_FILE"; then
-    # 기존 <VirtualHost *:80> 블록 찾기
-    sudo sed -i '/<VirtualHost \*:80>/,/<\/VirtualHost>/c\'"$BLOCK" "$CONFIG_FILE"
+if grep -q "<VirtualHost \*:80>" "$CONFIG_FILE"; then
+    # 기존 <VirtualHost *:80> 블록 찾기 및 대체
+    sudo sed -i "/<VirtualHost \*:80>/,/<\/VirtualHost>/c\\$BLOCK" "$CONFIG_FILE"
     echo "Existing <VirtualHost *:80> block replaced in $CONFIG_FILE"
 else
     echo "$BLOCK" | sudo tee -a "$CONFIG_FILE" > /dev/null
